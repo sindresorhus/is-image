944ab5f6a713165a62dfe8f9107e92c56ce076d8
FlyersWeb <contact@flyers-web.org>
2017-03-31 17:29:09 +0200

    use file type to determine file extension

--- test.js
+++ test.js
@@ -2,9 +2,10 @@ import test from 'ava';
 import m from './';
 
 test(t => {
-	t.true(m('unicorn.jpg'));
-	t.true(m('unicorn.JPG'));
-	t.false(m('unicorn.zip'));
-	t.false(m('unicornzip'));
-	t.false(m('unicorn.txt'));
+  m('unicorn.png').then(t.true)
+  m('unicorn.PNG').then(t.true)
+  m('https://upload.wikimedia.org/wikipedia/en/4/45/One_black_Pixel.png').then(t.true)
+  m('unicorn.zip').then(t.false)
+  m('unicornzip').then(t.false)
+  m('unicorn.txt').then(t.false)
 });
