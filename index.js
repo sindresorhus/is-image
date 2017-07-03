f83e069d4c57f9cd36e28f434d1ea230b50f176e
Flyers <contact@flyers-web.org>
2017-07-04 00:17:24 +0200

    Update tests to manage promises

--- index.js
+++ index.js
@@ -6,32 +6,24 @@ var fileType = require('file-type');
 var readChunk = require('read-chunk');
 var fetch = require('node-fetch');
 
-function checkFileType(data, exts) {
-  return fileType(data).ext in exts;
-}
+function checkFileType(data, exts) { return fileType(data).ext in exts; }
 
 function generateExtensions() {
   var exts = Object.create(null);
-  imageExtensions.forEach(function (el) {
-    exts[el] = true;
-  });
+  imageExtensions.forEach(function(el) { exts[el] = true; });
   return exts;
 }
 
-module.exports = function (filepath) {
+module.exports = function(filepath) {
   var exts = generateExtensions();
 
-  if(isUrl(filepath)) {
+  if (isUrl(filepath)) {
     return fetch(filepath)
-      .then(function(res) {
-        return res.buffer();
-      })
-      .then(function(buffer) {
-        return checkFileType(buffer, exts)
-      })
+        .then(function(res) { return res.buffer(); })
+        .then(function(buffer) { return checkFileType(buffer, exts) })
+        .catch(function(error) { return false; })
   }
-  return readChunk(filepath, 0, 4100)
-    .then(function(buffer) {
-      return checkFileType(buffer, exts)
-    });      
+  return readChunk(filepath.toLowerCase(), 0, 4100)
+      .then(function(buffer) { return checkFileType(buffer, exts) })
+      .catch(function(error) { return false; });
 };
--- test.js
+++ test.js
@@ -1,11 +1,28 @@
 import test from 'ava';
 import m from './';
 
-test(t => {
-  m('unicorn.png').then(t.true)
-  m('unicorn.PNG').then(t.true)
-  m('https://upload.wikimedia.org/wikipedia/en/4/45/One_black_Pixel.png').then(t.true)
-  m('unicorn.zip').then(t.false)
-  m('unicornzip').then(t.false)
-  m('unicorn.txt').then(t.false)
+test(async t => {
+  const r = await m('unicorn.png');
+  t.true(r);
+});
+test(async t => {
+  const r = await m('unicorn.PNG');
+  t.true(r);
+});
+test(async t => {
+  const r = await m(
+      'https://upload.wikimedia.org/wikipedia/en/4/45/One_black_Pixel.png');
+  t.true(r);
+});
+test(async t => {
+  const r = await m('unicorn.zip');
+  t.false(r);
+});
+test(async t => {
+  const r = await m('unicornzip');
+  t.false(r);
+});
+test(async t => {
+  const r = await m('unicorn.txt');
+  t.false(r);
 });
