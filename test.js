d8cf3575ea515ec1aa651fc8813274b22dafe24e
Sindre Sorhus <sindresorhus@gmail.com>
2022-07-06 15:13:34 +0200

    Support HEIC images
    Fixes #12

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ import isImage from '.';
 test('main', t => {
 	t.true(isImage('unicorn.jpg'));
 	t.true(isImage('unicorn.JPG'));
+	t.true(isImage('unicorn.HEIC'));
 	t.false(isImage('unicorn.zip'));
 	t.false(isImage('unicornzip'));
 	t.false(isImage('unicorn.txt'));
